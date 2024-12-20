c1e211d0fa3f3060672506829824194a50662111
Darcy Campbell <darcycampbellatwork@outlook.com>
2024-12-20 14:15:00 +0800

    Before fetch update

--- src/components/LandingPage/HomePage/TextboxContact/index.jsx
+++ src/components/LandingPage/HomePage/TextboxContact/index.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 
 const TextboxContact = ({ show }) => {
-  const displaySetting = [{ display: "none" }, { display: "flex",   flexDirection: "column",
-    justifyContent: "space-between" }];
+  const displaySetting = [
+    { display: "none" },
+    {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+    },
+  ];
 
   return (
     <div
       className="side textbox container"
       style={show ? displaySetting[1] : displaySetting[0]}
     >
-      <p className="first component">Arrange a <span className="bold-text">free consultation</span> to start your next project.</p>
-      <ul className="second component">
+       
+      <p className="first component">
+        Arrange a <span className="bold-text">free consultation</span> to start
+        your next project.
+      </p>
+      <ul className="middle component list">
         <li>Call Steve on 0000 000 000</li>
         <li>Email me@medesign.com.au</li>
         <li>Find me at this address</li>
         <li>What is this?</li>
       </ul>
-      <p className="third component">I look forward to working with you!</p>
+      <p className="final component">I look forward to working with you!</p>
     </div>
   );
 };
--- src/components/LandingPage/HomePage/TextboxMain/index.jsx
+++ src/components/LandingPage/HomePage/TextboxMain/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 
-const TextboxMain = ({ clickedServices, clickedContact }) => {
+const TextboxMain = ({ clickedServices, clickedContact, isReady }) => {
   const paragraphText =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamu iaculis purus at facilisis malesuada. Donec sagittis arcu mi, vitae euismod diam placerat quis. Fusce vestibulum eleifend lectus, sed cursus nulla cursus in. Ut ac mi feugiat, tincidunt metus at, ultricies tellus. Nunc varius justo sed libero faucibus gravida. ";
 
@@ -14,7 +14,7 @@ const TextboxMain = ({ clickedServices, clickedContact }) => {
       </p>
       <div className="button container">
         <button onClick={clickedServices}>Services</button>
-        <button onClick={clickedContact}>Contact</button>
+        <button onClick={clickedContact} disabled={!isReady}>Contact</button>
       </div>
     </div>
   );
