274f0541b3c8df02dc77c37b052b6911a56c7846
Darcy Campbell <darcycampbellatwork@outlook.com>
2024-12-21 09:54:39 +0800

    Frontend working again

--- src/components/LandingPage/HomePage/TextboxContact/index.jsx
+++ src/components/LandingPage/HomePage/TextboxContact/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const TextboxContact = ({ show, details }) => {
+  var phone = "";
+  var email = "";
+  var address = "";
+  if (details) {
+    phone = details.phone;
+    email = details.email;
+    address = details.address;
+  }
   const displaySetting = [
     { display: "none" },
     {
@@ -15,15 +23,14 @@ const TextboxContact = ({ show, details }) => {
       className="side textbox container"
       style={show ? displaySetting[1] : displaySetting[0]}
     >
-       
       <p className="first component">
         Arrange a <span className="bold-text">free consultation</span> to start
         your next project.
       </p>
       <ul className="middle component list">
-        <li>Call Steve on ${details.phone}</li>
-        <li>Email ${details.email}</li>
-        <li>Find me at ${details.address}</li>
+        <li>Call Steve on ${phone}</li>
+        <li>Email ${email}</li>
+        <li>Find me at ${address}</li>
         <li>What is this?</li>
       </ul>
       <p className="final component">I look forward to working with you!</p>
